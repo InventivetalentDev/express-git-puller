cda44ad967b2b606ea9c3f90f9150fc331538696
inventivetalent <mail@inventivetalent.org>
2021-06-06 01:21:09 +0200

    move stuff

--- src/Puller.ts
+++ src/Puller.ts
@@ -2,8 +2,7 @@ import { EventEmitter } from "events";
 import { Request, Response, NextFunction, RequestHandler, Application } from "express";
 import { PathParams } from "express-serve-static-core";
 import { exec } from "child_process";
-import * as crypto from "crypto";
-import { GithubBody, GithubHeaders } from "./Github";
+import { GithubWebhook, WebhookOptions, GithubBody, GithubHeaders } from "@inventivetalent/express-github-webhook";
 
 const TAG = "[ExpressGitPuller] ";
 
@@ -48,18 +47,7 @@ type OptionVars = { [s: string]: string };
 type OptionCommands = { [s: string]: string[] };
 type OptionDelays = { [s: string]: number };
 
-export interface Options {
-    /**
-     * Webhook events to react to<br/>
-     * Defaults to <code>[push]</code>
-     */
-    events?: string[];
-
-    /**
-     * Secret to validate github webhooks
-     */
-    secret?: string;
-
+export interface Options extends WebhookOptions {
     /**
      * Additional token to check in query params (https://example.com/git_hook?token=12345)
      */
@@ -77,30 +65,6 @@ export interface Options {
      * </code>
      */
     vars?: OptionVars;
-
-    /**
-     * Ignore pushers with names matching this regex<br/>
-     * Defaults to <code>/\[bot\]/i</code>
-     */
-    pusherIgnoreRegex?: RegExp;
-
-    /**
-     * Ignore commit messages matching this regex<br/>
-     * Defaults to <code>/\[nopull\]/i</code>
-     */
-    commitIgnoreRegex?: RegExp;
-
-    /**
-     * Branches to react to<br/>
-     * Defaults to <code>[main, master]</code>
-     */
-    branches?: string[];
-
-    /**
-     * Whether to require tags to pull anything<br/>
-     * Defaults to <code>false</code>
-     */
-    onlyTags?: boolean;
 
     /**
      * Order in which to run the command categories in <code>commands</code><br/>
@@ -180,6 +144,7 @@ interface PullerEventEmitter {
 export class Puller extends EventEmitter implements PullerEventEmitter {
 
     private readonly _options: Options;
+    private readonly webhookHandler: GithubWebhook;
 
     constructor(options?: Options) {
         super();
@@ -187,6 +152,8 @@ export class Puller extends EventEmitter implements PullerEventEmitter {
         this._options.vars = { ...DEFAULT_OPTIONS.vars, ...options.vars };
         this._options.commands = { ...DEFAULT_OPTIONS.commands, ...options.commands };
         this._options.delays = { ...DEFAULT_OPTIONS.delays, ...options.delays };
+
+        this.webhookHandler = new GithubWebhook(options);
     }
 
     //// PUBLIC STUFF
@@ -197,7 +164,9 @@ export class Puller extends EventEmitter implements PullerEventEmitter {
 
     get middleware(): RequestHandler {
         return (req: Request, res: Response, next: NextFunction) => {
-            this.handleRequest(req, res);
+            this.webhookHandler.middleware(req,res, ()=>{
+                this.handleRequest(req, res);
+            });
         };
     }
 
@@ -210,39 +179,12 @@ export class Puller extends EventEmitter implements PullerEventEmitter {
     // OPTION CHECKS
 
     protected handleRequest(req: Request, res: Response): boolean {
-        if (!this.validateRequest(req, res)) {
-            return false;
-        }
-
         const body = req.body as GithubBody;
         const headers = req.headers as GithubHeaders;
-
-        if (!this.shouldHandleEvent(body, headers, req)) {
-            res.status(200).send();
-            return false;
-        }
-        if (!this.shouldHandleRef(body, headers, req)) {
-            res.status(200).send();
-            return false;
-        }
-        if (!this.shouldHandlePusher(body, headers, req)) {
-            res.status(200).send();
-            return false;
-        }
-        if (!this.shouldHandleCommit(body, headers, req)) {
-            res.status(200).send();
-            return false;
-        }
 
         if (!this.validateToken(body, headers, req)) {
             res.status(401).send("invalid token");
             console.warn(TAG + "Received webhook request with invalid token");
-            return false;
-        }
-
-        if (!this.validateSignature(body, headers, req)) {
-            res.status(401).send("invalid signature");
-            console.warn(TAG + "Received webhook request with invalid signature");
             return false;
         }
 
@@ -265,47 +207,6 @@ export class Puller extends EventEmitter implements PullerEventEmitter {
         return true;
     }
 
-    protected shouldHandleEvent(body: GithubBody, headers: GithubHeaders, req: Request): boolean {
-        if (this._options.events.length <= 0) return false;
-        if (this._options.events[0] === "*") return true;
-        const event = headers["x-github-event"] as string;
-        return this._options.events.includes(event);
-    }
-
-    protected shouldHandleRef(body: GithubBody, headers: GithubHeaders, req: Request): boolean {
-        if (this._options.branches.length <= 0) return false;
-        if (this._options.branches[0] === "*") return true;
-        if (this._options.onlyTags) {
-            // body.ref has tag/head info
-            if (!body.ref.startsWith("refs\/tags\/")) return false; // not a tag
-        }
-        // body.base_ref exists if there's a tag in body.ref
-        if (!body.ref && !body.base_ref) return false;
-        const branch = (body.base_ref || body.ref).replace(/refs\/heads\//, "");
-        return this._options.branches.includes(branch.trim());
-    }
-
-    protected shouldHandlePusher(body: GithubBody, headers: GithubHeaders, req: Request): boolean {
-        if (!this._options.pusherIgnoreRegex) return true;
-        if (!body.pusher) return true;
-        if (body.pusher.name && this._options.pusherIgnoreRegex.test(body.pusher.name)) {
-            // ignored
-            return false;
-        }
-        return true;
-    }
-
-    protected shouldHandleCommit(body: GithubBody, headers: GithubHeaders, req: Request): boolean {
-        if (!this._options.commitIgnoreRegex) return true;
-        if (!body.head_commit && !body.commits) return true;
-        const commit = body.head_commit || body.commits[0];
-        if (commit && this._options.commitIgnoreRegex.test(commit.message)) {
-            // ignore
-            return false;
-        }
-        return true;
-    }
-
     // VALIDATIONS
 
     protected validateToken(body: GithubBody, headers: GithubHeaders, req: Request): boolean {
@@ -314,50 +215,6 @@ export class Puller extends EventEmitter implements PullerEventEmitter {
         if (!token || token.length <= 0) return false;
         // check token
         return token === this._options.token
-    }
-
-    protected validateSignature(body: GithubBody, headers: GithubHeaders, req: Request): boolean {
-        if (!this._options.secret || this._options.secret.length <= 0) return true; // no secret configured
-        const signature = headers["x-hub-signature"] as string;
-        if (!signature || signature.length <= 0) return false;
-        // validate signature (https://gist.github.com/stigok/57d075c1cf2a609cb758898c0b202428)
-        const bodyStr = JSON.stringify(req.body);
-        const hmac = crypto.createHmac('sha1', this._options.secret);
-        const digest = Buffer.from("sha1=" + hmac.update(bodyStr).digest('hex'), 'utf8');
-        const checksum = Buffer.from(signature, 'utf8');
-        if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
-            console.warn(TAG + "Received webhook request with invalid signature (Body " + digest + " did not match header " + checksum + ")");
-            return false;
-        }
-        return true;
-    }
-
-    protected validateRequest(req: Request, res: Response): boolean {
-        if (req.method.toLowerCase() !== "post") {
-            res.status(400).send("invalid request method");
-            return false;
-        }
-        if (!req.headers["user-agent"] || req.headers["user-agent"].length <= 0) {
-            res.status(400).send("missing user agent header");
-            return false;
-        }
-        if (!req.headers["user-agent"].startsWith("GitHub-Hookshot/")) {
-            res.status(400).send("invalid user agent");
-            return false;
-        }
-        if (!req.headers["x-hub-signature"] || req.headers["x-hub-signature"].length <= 0) {
-            res.status(400).send("missing request signature");
-            return false;
-        }
-        if (!req.headers["x-github-event"] || req.headers["x-github-event"].length <= 0) {
-            res.status(400).send("missing event");
-            return false;
-        }
-        if (!req.body || req.body.length <= 0) {
-            res.status(400).send("missing body");
-            return false;
-        }
-        return true;
     }
 
     //// UTIL
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 export * from "./Puller";
-export * from "./Github";
